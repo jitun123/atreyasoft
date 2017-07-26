cb6933137d4cc30c45fb84a85376188e8e24b3e9
Jitun Bisoy <jbisoy@gmail.com>
2017-07-27 02:02:26 +0530

    changed

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('INDEX PAGE');
+  res.render('index.html')
 });
 
 module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.send('Main index API');
+});
+
 router.get('/tasks', (req, res, next) => {
   res.send('Task API');
+});
+
+
+router.get('/products/detail', (req, res, next) => {
+  res.send('Product Detail API');
+});
+
+
+router.get('/products', (req, res, next) => {
+  res.send('product API');
 });
 
 module.exports = router;
